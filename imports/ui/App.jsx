961225b9551eb080f99100fe97242ebb2e93e424
Kyle Beard <kylebeard@nuancedmedia.com>
2016-05-12 19:09:40 -0700

    changing tasks to store ui component state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			hideCompleted: false
+		};
+	}
+
 	handleSubmit(event) {
 		event.preventDefault();
 
@@ -22,8 +30,18 @@ class App extends Component {
 		ReactDOM.findDOMNode(this.refs.textInput).value = '';
 	}
 
+	toggleHideCompleted() {
+		this.setState({
+			hideCompleted: !this.state.hideCompleted
+		});
+	}
+
 	renderTasks() {
-		return this.props.tasks.map((task) => (<Task key={task._id} task={task}/>));
+		let filteredTasks = this.props.tasks;
+		if (this.state.hideCompleted) {
+			filteredTasks = filteredTasks.filter(task => !task.checked);
+		}
+		return filteredTasks.map((task) => (<Task key={task._id} task={task}/>));
 	}
 
 	render() {
@@ -31,6 +49,11 @@ class App extends Component {
 			<div className="container">
 				<header>
 					<h1>Todo List</h1>
+
+					<label className="hide-completed">
+						<input type="checkbox" readOnly checked={this.state.hideCompleted} onClick={this.toggleHideCompleted.bind(this)}/>
+						Hide Completed Tasks
+					</label>
 
 					{/* form to add tasks */}
 					<form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
@@ -51,5 +74,9 @@ App.propTypes = {
 };
 
 export default createContainer(() => {
-	return {tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()};
+	return {tasks: Tasks.find({}, {
+			sort: {
+				createdAt: -1
+			}
+		}).fetch()};
 }, App);
